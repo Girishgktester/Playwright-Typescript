3309e8ec08216f30a6f184482aa5c82c1f9bb1b1
Girishgktester <girishgk.tester@gmail.com>
2026-03-23 16:06:55 +0530

    Fixed search

--- tests/smoke/search.spec.ts
+++ tests/smoke/search.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@fixtures/objectsFixtures';
 import { testData } from '@test-data/users';
 
-test('Search with valid keyword', async ({ basePage, loginAssertion, searchPage, searchAssertion }) => {
+test('Search with valid keyword', async ({ basePage, searchPage, searchAssertion }) => {
   await basePage.goto("https://demowebshop.tricentis.com/");
   await searchPage.inputSearch(testData.search.searchKey1);
   await searchAssertion.verifySearchResult(testData.search.searchKey1)
