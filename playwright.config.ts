d8593c6773e404de34a0c29d19279ac944eecce7
Girishgktester <girishgk.tester@gmail.com>
2026-03-25 17:47:18 +0530

    Finally

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,66 +1,36 @@
 import { defineConfig, devices } from '@playwright/test';
-import 'tsconfig-paths/register';
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// import { ENV } from './config/env';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   globalSetup: './global-setup.ts',
+
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
+
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 1 : undefined,
+
   reporter: 'html',
 
-    grep: /@smoke/,   // run only smoke
+  grep: /@smoke/,
 
   use: {
-    baseURL: 'http://localhost:3000',
+    // baseURL: ENV.baseURL,
     // storageState: 'storageState.json',
 
     launchOptions: {
-      slowMo: 900,
+      slowMo: 100,
     },
 
     trace: 'on-first-retry',
     headless: !!process.env.CI,
-    channel: 'chrome'
-
   },
 
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-
-    // },
-
-    /* Test against branded browsers. */
     {
       name: 'Microsoft Edge',
       use: { ...devices['Desktop Edge'], channel: 'msedge' },
     },
-  
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-
-
-
-
 });
-
-
